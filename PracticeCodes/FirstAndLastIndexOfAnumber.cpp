842a48d196599250753ff98c5be8148168d6f5fa
NoobAnu <anubhavg106@gmail.com>
2022-05-24 18:02:21 +0530

    Add

--- PracticeCodes/FirstAndLastIndexOfAnumber.cpp
+++ PracticeCodes/FirstAndLastIndexOfAnumber.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
 using namespace std;
 
+int firstOccurence(int arr[],int size, int key){
+int s=0;
+int e=size-1;
+int mid= s + (e-s)/2;
+int firstOccurence;
 
+while(s<=e){
+if(key==arr[mid]){
+firstOccurence=mid;
+e=mid-1;
+}
+
+if(key>arr[mid]){
+    s=mid+1;
+}
+
+if(key<arr[mid]){
+    e=mid-1;
+}
+
+
+}
+
+return firstOccurence;
+
+}
 
 
 
@@ -9,13 +34,12 @@ using namespace std;
 
 int main(){
 
-int n;
-int arr[n];
 
-for(int i=0;i<n; i++){
-cin >> arr[i];
+int arr[5]={12,13,13,23,45};
 
-}
+int firstIndex = firstOccurence(arr,5,13);
+
+cout << firstIndex;
 
     return 0;
 }
