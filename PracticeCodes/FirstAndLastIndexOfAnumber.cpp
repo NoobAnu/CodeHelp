645638252752d78d78b34983aecf7291d8604923
NoobAnu <anubhavg106@gmail.com>
2022-05-25 16:45:54 +0530

    Adding Soln

--- PracticeCodes/FirstAndLastIndexOfAnumber.cpp
+++ PracticeCodes/FirstAndLastIndexOfAnumber.cpp
@@ -1,30 +1,57 @@
 #include<iostream>
 using namespace std;
 
-int firstOccurence(int arr[],int size, int key){
+int firstOccurence(int arr[],int size, int k){
 int s=0;
 int e=size-1;
 int mid= s + (e-s)/2;
-int firstOccurence;
+int firstIndex;
 
 while(s<=e){
-if(key==arr[mid]){
-firstOccurence=mid;
+if(k==arr[mid]){
+firstIndex=mid;
 e=mid-1;
 }
 
-if(key>arr[mid]){
+if(k>arr[mid]){
     s=mid+1;
 }
 
-if(key<arr[mid]){
+if(k<arr[mid]){
     e=mid-1;
 }
 
-
+mid = s + (e-s)/2;
 }
 
-return firstOccurence;
+return firstIndex;
+
+}
+int lastOccurence(int arr[],int n, int k){
+int s=0;
+int e=n-1;
+int mid= s + (e-s)/2;
+int lastIndex =firstOccurence(arr,n,k);
+s=lastIndex+1;
+
+while(s<=e){
+if(k==arr[mid]){
+lastIndex=mid;
+s=mid+1;
+}
+
+if(k>arr[mid]){
+    s=mid+1;
+}
+
+if(k<arr[mid]){
+    e=mid-1;
+}
+
+mid = s + (e-s)/2;
+}
+
+return lastIndex;
 
 }
 
@@ -35,11 +62,12 @@ return firstOccurence;
 int main(){
 
 
-int arr[5]={12,13,13,23,45};
+int arr[5]={12,13,13,13,13};
 
 int firstIndex = firstOccurence(arr,5,13);
-
-cout << firstIndex;
+int lastIndex = lastOccurence(arr,5,13);
+cout << firstIndex << endl;
+cout << lastIndex << endl;
 
     return 0;
 }
