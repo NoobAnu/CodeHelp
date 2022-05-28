a75b0aacb8e8e05a08d84e3e7768d8265f5b0ee0
NoobAnu <anubhavg106@gmail.com>
2022-05-28 20:08:28 +0530

    vscode

---
+++ Lecture21_MoreArrayQ's/RotateArray2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void printArray(vector<int> &arr){
+    for(int i: arr)
+     cout << i << "  ";
+}
+
+  void rotateArray(vector<int>& arr, int m) {
+        
+        vector<int> temp(nums.size());
+        
+        for(int i=0;i<nums.size();i++){
+            temp[(i+k)%nums.size()]=nums[i];
+        }
+        nums =temp;
+    }
+
+int main(){
+vector<int> arr={1,2,3,4,5,6,7,8};
+
+rotateArray(arr,3);
+printArray(arr);
+
+    return 0;
+}
